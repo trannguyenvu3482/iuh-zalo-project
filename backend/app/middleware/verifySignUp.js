b5fe551797961f274fe9d2913365f6317380576e
Vu Tran <21088201.vu@student.iuh.edu.vn>
2025-02-21 03:48:06 +0700

    feat: working on auth and message nodejs backend

---
+++ backend/app/middleware/verifySignUp.js
@@ -0,0 +1,70 @@
+const db = require("../models");
+const ROLES = db.ROLES;
+const User = db.user;
+
+checkDuplicateUsernameOrEmail = (req, res, next) => {
+  // Check if fields are empty
+  if (!req.body.username || !req.body.email) {
+    return res.status(400).send({
+      message: "Failed! Please provide username and email!",
+    });
+  }
+
+  // Username
+  User.findOne({
+    where: {
+      username: req.body.username,
+    },
+  }).then((user) => {
+    if (user) {
+      res.status(400).send({
+        message: "Failed! Username is already in use!",
+      });
+      return;
+    }
+
+    // Email
+    User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    }).then((user) => {
+      if (user) {
+        res.status(400).send({
+          message: "Failed! Email is already in use!",
+        });
+        return;
+      }
+
+      next();
+    });
+  });
+};
+
+const checkRolesExisted = (req, res, next) => {
+  // Check if fields are empty
+  if (!req.body.roles) {
+    return res.status(400).send({
+      message: "Failed! Please provide roles!",
+    });
+  }
+
+  if (req.body.roles) {
+    for (let i = 0; i < req.body.roles.length; i++) {
+      if (!ROLES.includes(req.body.roles[i])) {
+        return res.status(400).send({
+          message: "Failed! Role does not exist = " + req.body.roles[i],
+        });
+      }
+    }
+  }
+
+  next();
+};
+
+const verifySignUp = {
+  checkDuplicateUsernameOrEmail,
+  checkRolesExisted,
+};
+
+module.exports = verifySignUp;
--- backend/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import type { FastifyInstance } from "fastify";
-import authRoutes from "./auth";
-import usersRoutes from "./users";
-
-export default async function registerRoutes(fastify: FastifyInstance) {
-  await fastify.register(usersRoutes);
-  await fastify.register(authRoutes);
-}
