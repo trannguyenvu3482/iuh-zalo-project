92574440ec00d1f6b8df3a03d496b7b30966ff73
Vu Tran <21088201.vu@student.iuh.edu.vn>
2025-02-21 04:07:13 +0700

    fix: mobile files

--- mobile/App.js
+++ mobile/App.js
@@ -1,33 +1,4 @@
-<<<<<<< HEAD
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import Home from "./src/navigation/Home";
 export default function App() {
   return <Home />;
 }
-=======
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-<<<<<<< HEAD
->>>>>>> cfd46bf (create mobile)
-=======
->>>>>>> 9e09406 (create mobile)
->>>>>>> 4ce08b6 (create mobile)
