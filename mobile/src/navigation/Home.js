61ee8084c39d552d558411c489cb43efa01ab8bd
Tran Ngoc Phat <tnphat203@gmail.com>
2025-04-02 12:25:48 +0700

    pull main

--- mobile/src/navigation/Home.js
+++ mobile/src/navigation/Home.js
@@ -10,8 +10,12 @@ const Stack = createNativeStackNavigator();
 const Home = ({ navigation }) => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Register">
-        <Stack.Screen name="Login" component={Login} />
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
         <Stack.Screen name="Register" component={Register} />
       </Stack.Navigator>
     </NavigationContainer>
--- mobile/src/screens/Login/Login.js
+++ mobile/src/screens/Login/Login.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-
+import { Picker } from "@react-native-picker/picker";
 const Login = () => {
+  const [selectedLanguage, setSelectedLanguage] = useState("vi");
+  const languages = [
+    { label: "Tiếng Việt", value: "vi" },
+    { label: "English", value: "en" },
+  ];
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={{ marginTop: 30, width: "100%" }}>
+      <View
+        style={{
+          width: "100%",
+          alignItems: "flex-end",
+        }}
+      >
+        <Picker
+          selectedValue={selectedLanguage}
+          style={{
+            height: 20,
+            width: 100,
+            backgroundColor: "red",
+            color: "white",
+          }}
+          onValueChange={(itemValue) => setSelectedLanguage(itemValue)}
+        >
+          <Picker.Item label="Vietnamese" value="vi" />
+          <Picker.Item label="English" value="en" />
+        </Picker>
+      </View>
     </View>
   );
 };
